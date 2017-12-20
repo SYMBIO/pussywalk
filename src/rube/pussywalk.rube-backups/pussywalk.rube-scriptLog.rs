b1a92036c100f41d402ad502041dfcd2b126d0c2
Páll Zoltán <zoltan.pall@firma.seznam.cz>
2017-12-20 02:14:22 +0100

    level end physics fix

--- src/rube/pussywalk.rube-backups/pussywalk.rube-scriptLog.rs
+++ src/rube/pussywalk.rube-backups/pussywalk.rube-scriptLog.rs
@@ -1,5 +1,1 @@
-//Restarted script log at Mon Dec 18 21:17:16 2017
-getFixture(38).deselect();
-getFixture(64).select();
-getBody(43).select();
-getBody(43).deselect();
+//Restarted script log at Wed Dec 20 02:12:52 2017
