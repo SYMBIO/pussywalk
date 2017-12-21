554d5c6cff5cb9c8711f04657f1489f8d1be4f40
Jiri Vana <ahoj@narezuti.cz>
2017-12-21 18:01:33 +0100

    sahre text

--- src/images/layout/share.php
+++ src/images/layout/share.php
@@ -5,8 +5,8 @@
 	<meta property="og:type" content="website">
 	<meta property="og:name" content="Si vyhrals name">
 	<meta property="og:url" content="http://www.pussywalk.com">
-	<meta property="og:description" content="Si vyhrals description, jmeno: <?php if(isset($_GET['n'])) { echo $_GET['n'] } ?>, cas: <?php if(isset($_GET['t'])) { echo $_GET['t'] } ?>">
-	<meta property="og:image" content="http://2.pussywalk.com/images/layout/fb-share.png">
+	<meta property="og:description" content="Si vyhrals description, jmeno: ">
+	<meta property="og:image" content="http://pussywalk.com/images/layout/fb-share.png">
 </head>
 <body>
 </body>
