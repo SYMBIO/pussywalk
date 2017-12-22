c73d459a9860c0b9b7a33210ab6e1c915f3527cd
Jiří Váňa <ahoj@narezuti.cz>
2017-12-22 18:14:31 +0100

    share

--- src/images/layout/share.php
+++ src/images/layout/share.php
@@ -4,7 +4,7 @@
 	<meta property="og:title" content="<?php if(isset($_GET['n'])) { echo $_GET['n']; } ?> zdolal/a PussyWalk II!">
 	<meta property="og:type" content="website">
 	<meta property="og:name" content="<?php if(isset($_GET['n'])) { echo $_GET['n']; } ?> zdolal/a PussyWalk II!">
-	<meta property="og:url" content="http://www.pussywalk.com">
+	<meta property="og:url" content="http://pussywalk.com/images/layout/share.php?n=<?php if(isset($_GET['n'])) { echo $_GET['n']; } ?>&t=<?php if(isset($_GET['n'])) { echo $_GET['n']; } ?>">
 	<meta property="og:description" content="Do cíle dovrávoral/a v čase <?php if(isset($_GET['t'])) { echo $_GET['t']; } ?>. Vem pana Z. a Čtveráčka taky na procházku a přibliž je k urně. Překonáš můj čas?">
 	<meta property="og:image" content="http://www.pussywalk.com/images/layout/fb-share-winner.png">
 </head>
