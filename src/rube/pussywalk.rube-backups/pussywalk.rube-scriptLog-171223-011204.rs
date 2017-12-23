952dc0093e06e3533dea810b2a2269bde820a985
Páll Zoltán <zoltan.pall@firma.seznam.cz>
2017-12-23 01:19:16 +0100

    sliding heels shape

---
+++ src/rube/pussywalk.rube-backups/pussywalk.rube-scriptLog-171223-011204.rs
@@ -0,0 +1,1 @@
+//Restarted script log at Sat Dec 23 01:12:04 2017
--- src/rube/pussywalk.rube-backups/pussywalk.rube-scriptLog.rs
+++ src/rube/pussywalk.rube-backups/pussywalk.rube-scriptLog.rs
@@ -1,1 +1,3 @@
-//Restarted script log at Sat Dec 23 00:33:28 2017
+//Restarted script log at Sat Dec 23 01:13:37 2017
+getFixture(78).select();
+getFixture(76).select();
