4a956b67e5bdbcd056784eca5f3c610ae7689795
Páll Zoltán <zoltan.pall@firma.seznam.cz>
2017-12-28 15:17:56 +0200

    test scoreboard

--- src/scoreboard.php
+++ src/scoreboard.php
@@ -1,25 +1,38 @@
 <?php
 
-$fburl = "https://pussywalk-2.firebaseio.com/scoreboard.json?orderBy=\"time\"&limitToFirst=2000";
+$fburl = "https://pussywalk-2.firebaseio.com/scoreboard.json?orderBy=\"time\"&limitToFirst=2";
 $dirname = "cache";
 $filename = "scoreboard.json";
 $filepath = $dirname . "/" . $filename;
 $loadNew = false;
 $now = time();
 
-$filedate = filemtime($filepath);
+echo "1";
 
 if (!file_exists($dirname)) {
     mkdir($dirname, 0777);
     $loadNew = true;
 }
 
-$loadNew = $loadNew || ($now - filemtime($filepath) > 60);
+echo "2";
+
+if (file_exists($filepath)) {
+  $loadNew = $loadNew || ($now - filemtime($filepath) > 60);
+} else {
+  $loadNew = true;
+}
+
+echo "3";
 
 if ($loadNew) {
+  echo "4";
   $json = file_get_contents($fburl);
+  echo "5";
   file_put_contents($filepath, $json);
+  echo "6";
 }
+
+echo "7";
 
 readfile($filepath);
 
