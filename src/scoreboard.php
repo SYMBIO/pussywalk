6fd8fa36dfc8703d03bcffd992a6e894dcc685a4
Páll Zoltán <zoltan.pall@firma.seznam.cz>
2017-12-29 18:12:09 +0200

    scoreboard cleanup (scoreboard.json is now 0666)

--- src/scoreboard.php
+++ src/scoreboard.php
@@ -3,13 +3,13 @@
 ini_set('display_errors', 1); // set to 0 for production version
 error_reporting(E_ALL);
 
-$fburl = "https://pussywalk-2.firebaseio.com/scoreboard.json?orderBy=\"time\"&limitToFirst=2";
+$fburl = "https://pussywalk-2.firebaseio.com/scoreboard.json?orderBy=\"time\"&limitToFirst=2000";
 $filename = "scoreboard.json";
 $loadNew = false;
 $now = time();
 
 if (file_exists($filename)) {
-  $loadNew = $loadNew || ($now - filemtime($filename) > 6);
+  $loadNew = $loadNew || ($now - filemtime($filename) > 60);
 } else {
   $loadNew = true;
 }
@@ -18,33 +18,13 @@ if ($loadNew) {
   $json = file_get_contents($fburl);
   $file = fopen($filename,"w");
 
-  chmod($filename, 0755);
-
-  echo realpath($filename);
-  echo "<br />";
-
-  echo substr(sprintf('%o', fileperms($filename)), -4);
-  echo "<br />";
-
   if (false === $file) {
-    echo "cant open file .(";
-    echo "<br />";
-  }
-$result = fwrite($file, $json);
-
-echo "result: " . $result;
-echo "<br />";
-
-  if($result) {
-    echo "written .)";
-    echo "<br />";
+    //
   } else {
-    echo "can't write .(";
-    echo "<br />";
+    $result = fwrite($file, $json);
+    fclose($file);
+    }
   }
-  fclose($file);
-}
 
-readfile($filename);
-
+  readfile($filename);
 ?>
